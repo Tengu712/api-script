53d3b26a7f10bd50af885e13b32a9e49181ca9d9
Tengu712 <65766435+Tengu712@users.noreply.github.com>
2022-05-16 18:48:28 +0900

    totyuu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+enum Token {
+    Fun,
+    Args,
+    Logic,
+    Call,
+    Ptr,
+    I32,
+    U32,
+}
+fn analyze_tokens(code: &String) -> Vec<Token> {
+    let v = Vec::new();
+    let tmp = String::new();
+    for c in code.chars() {
+        match c {
+            ' ' | '\n' => {
+                if c == ' ' {
+                    println!("space");
+                } else {
+                    println!("newline");
+                }
+            }
+            _ => println!("{}", c),
+        }
+    }
+    v
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() < 2 {
+        println!("fatal error: no file input.");
+        return;
+    }
+    for i in args {
+        
+    }
 }
