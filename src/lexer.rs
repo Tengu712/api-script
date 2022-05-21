2e95004e42488caf774cb484b60ad0a4f60e0487
Tengu712 <genreihoutengu@gmail.com>
2022-05-21 19:58:58 +0900

    create lexer

---
+++ src/lexer.rs
@@ -0,0 +1,119 @@
+#[derive(Debug)]
+pub enum Token {
+    // System
+    Indent,
+    Outdent,
+    // Symbol
+    Fun,
+    Args,
+    Logic,
+    Call,
+    Ptr,
+    I32,
+    U32,
+    // Data
+    Str(String),
+    Int(String),
+    Float(String),
+    Id(String),
+}
+impl Token {
+    fn from(w: &str) -> Self {
+        use Token::*;
+        match w {
+            "fun" => Fun,
+            "args" => Args,
+            "logic" => Logic,
+            "call" => Call,
+            "ptr" => Ptr,
+            "i32" => I32,
+            "u32" => U32,
+            _ => {
+                let mut chars = w.chars();
+                if chars.nth(0).unwrap() == '\"' && chars.nth_back(0).unwrap() == '\"' {
+                    Str(String::from(w))
+                } else if is_integer_literal(w) {
+                    Int(String::from(w))
+                } else if is_float_literal(w) {
+                    Float(String::from(w))
+                } else {
+                    Id(String::from(w))
+                }
+            }
+        }
+    }
+}
+fn is_integer_literal(trg: &str) -> bool {
+    trg.parse::<i64>().is_ok() || trg.parse::<u64>().is_ok()
+}
+fn is_float_literal(trg: &str) -> bool {
+    trg.parse::<f64>().is_ok()
+}
+/// A function to split line considering string literal
+/// and get number of spaces that's at the top of the line.
+fn split_line(trg: String) -> (Vec<String>, usize) {
+    let mut v = Vec::new();
+    let mut buf = String::new();
+    let mut num_spaces = 0;
+    let mut is_started = false;
+    let mut is_in_word = false;
+    let mut is_literal = false;
+    for c in trg.chars() {
+        if c == '#' {
+            break;
+        }
+        if c == '\"' {
+            is_literal = !is_literal;
+        }
+        if c == ' ' {
+            if is_in_word {
+                if is_literal {
+                    buf.push(c);
+                } else {
+                    v.push(buf.clone());
+                    is_in_word = false;
+                }
+            } else if !is_started {
+                num_spaces += 1;
+            }
+            continue;
+        }
+        if !is_in_word {
+            is_started = true;
+            is_in_word = true;
+            buf.clear();
+        }
+        buf.push(c);
+    }
+    if buf.len() > 0 {
+        v.push(buf.clone());
+    }
+    (v, num_spaces)
+}
+pub fn analyze_tokens_from_file(path: &String) -> Vec<Token> {
+    // Open file
+    use std::{
+        fs::File,
+        io::{BufRead, BufReader},
+    };
+    let reader = BufReader::new(File::open(path).unwrap());
+    // Analyze each line
+    let mut v = Vec::new();
+    let mut indent = 0;
+    for line_opt in reader.lines() {
+        let line = line_opt.unwrap();
+        let (words, num_spaces) = split_line(line);
+        if words.len() > 0 {
+            if num_spaces > indent {
+                v.push(Token::Indent);
+            } else if num_spaces < indent {
+                v.push(Token::Outdent);
+            }
+            indent = num_spaces;
+        }
+        for w in words {
+            v.push(Token::from(&w));
+        }
+    }
+    v
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,15 @@
-enum Token {
-    Fun,
-    Args,
-    Logic,
-    Call,
-    Ptr,
-    I32,
-    U32,
-}
-fn analyze_tokens(code: &String) -> Vec<Token> {
-    let v = Vec::new();
-    let mut tmp = String::new();
-    let mut indent = 0;
-    for c in code.chars() {
-        match c {
-            _ => tmp.push(c),
-        }
-    }
-    v
-}
+mod lexer;
 
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
     if args.len() < 2 {
-        println!("fatal error: no file input.");
+        println!("[fatal error] no file input.");
         return;
     }
-    for i in args {
-        println!("{}", i);
-        analyze_tokens(&i);
+    let mut itr = args.iter();
+    let _ = itr.next();
+    for i in itr {
+        let tokens = lexer::analyze_tokens_from_file(&i);
+        println!("{:?}", tokens);
     }
 }
